8275f714ea6b49b63a87cf7e056ec311ffbd9719
yuyaohshimo <g2110008@gmail.com>
2017-03-06 23:24:14 +0900

    First commit

---
+++ src/HoT.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import numbro from 'numbro';
+import moment from 'moment';
+import pikaday from 'pikaday';
+import Zeroclipboard from 'zeroclipboard';
+import Handsontable from 'handsontable';
+
+class HoT extends Component {
+  componentDidMount() {
+    if (!this.hot) {
+      this.hot = new Handsontable(this._hotWrapper, {
+        data: [
+          ['', 'Kia', 'Nissan', 'Toyota', 'Honda', 'Mazda', 'Ford'],
+          ['2012', 10, 11, 12, 13, 15, 16],
+          ['2013', 10, 11, 12, 13, 15, 16],
+          ['2014', 10, 11, 12, 13, 15, 16],
+          ['2015', 10, 11, 12, 13, 15, 16],
+          ['2016', 10, 11, 12, 13, 15, 16]
+        ],
+        width: 400,
+        height: 400,
+      });
+    }
+  }
+
+  render() {
+    return <div>
+    <div>Handsontable</div>
+    <div ref={(c) => this._hotWrapper = c}></div>
+    </div>;
+  }
+}
+
+export default HoT;
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['react-hot', 'babel'],
+        include: path.join(__dirname, 'src')
+      }, {
+        test: require.resolve('numbro'),
+        loader: 'expose?numbro'
+      }, {
+        test: require.resolve('moment'),
+        loader: 'expose?moment'
+      }, {
+        test: require.resolve('pikaday'),
+        loader: 'expose?Pikaday'
+      }, {
+        test: require.resolve('zeroclipboard'),
+        loader: 'expose?ZeroClipboard'
+      }
+    ]
+  }
+};
